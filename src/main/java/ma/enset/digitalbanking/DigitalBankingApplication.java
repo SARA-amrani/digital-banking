aa5a6337d6876b10cdeadba76c72c7c8ade76965
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-05-10 13:02:15 +0100

    Initialize sample customers and bank accounts

--- src/main/java/ma/enset/digitalbanking/DigitalBankingApplication.java
+++ src/main/java/ma/enset/digitalbanking/DigitalBankingApplication.java
@@ -1,13 +1,60 @@
 package ma.enset.digitalbanking;
 
+import ma.enset.digitalbanking.entities.CurrentAccount;
+import ma.enset.digitalbanking.entities.Customer;
+import ma.enset.digitalbanking.entities.SavingAccount;
+import ma.enset.digitalbanking.enums.AccountStatus;
+import ma.enset.digitalbanking.repositories.AccountOperationRepository;
+import ma.enset.digitalbanking.repositories.BankAccountRepository;
+import ma.enset.digitalbanking.repositories.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class DigitalBankingApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(DigitalBankingApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner start(CustomerRepository customerRepository,
+                            BankAccountRepository bankAccountRepository,
+                            AccountOperationRepository accountOperationRepository) {
+        return args -> {
+            Stream.of("Sara","Houda","Aicha").forEach(name -> {
+                Customer customer = new Customer();
+                customer.setName(name);
+                customer.setEmail(name + "@gmail.com");
+                customerRepository.save(customer);
+            });
+            customerRepository.findAll().forEach(customer -> {
+                CurrentAccount currentAccount = new CurrentAccount();
+                currentAccount.setCustomer(customer);
+                currentAccount.setBalance(Math.random() * 90000);
+                currentAccount.setCreateDate(new Date());
+                currentAccount.setStatus(AccountStatus.CREATED);
+                currentAccount.setOverDraft(9000);
+                bankAccountRepository.save(currentAccount);
+
+
+                SavingAccount savingAccount = new SavingAccount();
+                savingAccount.setCustomer(customer);
+                savingAccount.setBalance(Math.random() * 90000);
+                savingAccount.setCreateDate(new Date());
+                savingAccount.setStatus(AccountStatus.CREATED);
+                savingAccount.setInterestRate(5.5);
+                bankAccountRepository.save(savingAccount);
+            });
+
+        };
+    }
+
 
 }
